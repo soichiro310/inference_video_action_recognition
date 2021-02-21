dd86d026e637c2ce8d9f4f310b2d9649da3c8739
Soichiro Sato <s-sato@gpu01.kde.cs.tut.ac.jp>
2021-02-21 10:49:24 +0900

    ボタンを押してページを遷移する部分を作成

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import torch
+import torch.nn as nn
+
 from flask import *
 from opts import get_parser
+from python_src.i3d import InceptionI3d
 
 app = Flask(__name__)
 
+model = InceptionI3d(400, in_channels=3)
+model.load_state_dict(torch.load('./model/rgb_imagenet.pt'))
+
 @app.route("/")
-def main():
-    return "Hello World"
+def redirectToIndex():
+    return redirect(url_for('renderIndex'))
+
+@app.route("/index", methods=['GET', 'POST'])
+def renderIndex():
+    return render_template("index.html")
+
+@app.route("/result", methods=['POST'])
+def renderResult():
+    return render_template("result.html")
 
 
 if __name__ == "__main__":
