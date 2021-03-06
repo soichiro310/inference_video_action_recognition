49a04d516f9e487af84ef144c46314d730fb21f7
soichiro310 <s183332@edu.tut.ac.jp>
2021-03-06 18:07:57 +0900

    選択したファイルに問題があった場合はエラーメッセージを表示させるようにした

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import numpy as np
 import time
 import glob
 import os
+import random
+import string
 
 from flask import *
 
@@ -11,12 +13,16 @@ from python_src.InferenceModel import InferenceModel
 
 def createApp(args):
     app = Flask(__name__)
+
+    # シークレットキー生成
+    app.secret_key = "".join([random.choice(string.ascii_letters + string.digits + '_' + '-' + '!' + '#' + '&') for i in range(64)])
+    
     # 推論モデルを定義
     cls_model = InferenceModel(model=I3D(),
                             weight_path='./data/weights/rgb_imagenet.pkl',
                             label_map_path='./data/label_map.txt',
                             )
-
+    
     # indexにリダイレクト
     @app.route("/")
     def redirectToIndex():
@@ -25,25 +31,30 @@ def createApp(args):
     @app.route("/index", methods=['GET', 'POST'])
     def renderIndex():
         video_list = [video_path.split('/')[-1] for video_path in glob.glob(os.path.join(args.sample_video_dir,'*.avi'))]
+
+        # 選択したファイルに問題があった場合はエラーメッセージを表示させる
+        error = session['error'] if 'error' in session else ''
+
         return render_template("index.html",
+                            error = error,
                             video_list = video_list
                             )
 
-    @app.route("/result", methods=['POST'])
-    def renderResult():
-        # プルダウンメニューで選択した動画のファイルパスを取得
+    @app.route('/post', methods=['POST'])
+    def dataCheck():
         try:
+            # プルダウンメニューで選択した動画のファイルパスを取得
             video_path = os.path.join(args.sample_video_dir,request.form.get('select_video'))
 
             # 動画ファイルの推論
             tic = time.time()
             pred = cls_model.inferenceVideo(video_path=video_path)
             toc = time.time()
-        except TypeError:
-            print('選択が誤っています')
+        except TypeError:   # プルダウンの初期値のまま，ボタンを押してしまうと起きてしまう例外
+            session['error'] = '動画ファイル(.avi)を選択してください．'
             return redirect(url_for('renderIndex'))
-        except Exception as e:
-            print(e)
+        except Exception as e:  # ファイルが開けなかった場合
+            session['error'] = '選択したファイル('+ request.form.get('select_video') +')が開けませんでした．'
             return redirect(url_for('renderIndex'))
 
         # 結果を表示させるための文字列をprint_results_strに格納してレンダリング
@@ -51,11 +62,21 @@ def createApp(args):
         print_results_str = []
         for index in sorted_indices[:10]:
             print_results_str.append('{}\t{} %'.format(cls_model.classes[index], round(pred[index]*100.0,3)))
-        
+
+        session['error'] = ''
+        session['video'] = request.form['select_video']
+        session['best_result'] = cls_model.classes[sorted_indices[0]]
+        session['time'] = round(toc-tic,3)
+        session['results'] = print_results_str
+
+        return redirect(url_for('renderResult'))
+
+    @app.route("/result", methods=['GET', 'POST'])
+    def renderResult():
         return render_template("result.html",
-                            best_result = cls_model.classes[sorted_indices[0]],
-                            time = round(toc-tic,3),
-                            results = print_results_str
+                            best_result = session['best_result'],
+                            time = session['time'],
+                            results = session['results']
                             )
 
     return app
